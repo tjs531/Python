31bc9caf3affb7bee05d6be6d53428e7363efb11
tjs531 <tjs531@naver.com>
2020-10-14 17:38:37 +0900

    2020-10-14

--- pymysql_test.py
+++ pymysql_test.py
@@ -12,10 +12,13 @@ print("DB접속성공!")
 try:
     with connection.cursor() as cursor:
         # Read a single record
-        sql = "SELECT `w_no`,`w_title` FROM `t_webtoon` WHERE `w_no`=%c"
-        cursor.execute(sql, (1))
-        result = cursor.fetchone()
-        print(result)
+        sql = "SELECT `w_no`,`w_title` FROM `t_webtoon` WHERE `w_no` in (%c,%c,%c)"
+        #sql = "SELECT * FROM `t_user`"
+        data = (1,2,3)
+        cursor.execute(sql,data)
+        result = cursor.fetchall()
+        for r in result:
+            print(r)
 finally:
     connection.close()
 
