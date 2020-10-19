b7bbd87e5e07134891f9d3b1127830b908d68184
tjs531 <tjs531@naver.com>
2020-10-19 10:23:52 +0900

    recommend

--- recommend_toon.py
+++ recommend_toon.py
@@ -12,9 +12,10 @@ def pearsonR(s1, s2):
 
 def recommend(input_toon, matrix, n, similar_genre=True):
     input_genres = result_webtoon[result_webtoon['w_no'] == input_toon]['genre_name'].iloc(0)[0].split(",")
-
+    
     result = []
     for no in matrix.columns:
+
         if no == input_toon:
             continue
 
@@ -24,13 +25,8 @@ def recommend(input_toon, matrix, n, similar_genre=True):
             temp_genres = result_webtoon[result_webtoon['w_no'] == no]['genre_name'].iloc(0)[0].split(",")
 
             same_count = np.count_nonzero(np.in1d(temp_genres, input_genres, assume_unique=True)== True)
-            #print("input_toon = " + str(input_toon))
-            #print("no = " + str(no))
-            #print("same_count = " + str(same_count))
 
-            #print('b : ' + str(cor))
             cor += (GENRE_WEIGHT * same_count)
-            #print('a : ' + str(cor))
 
         if str(cor)=='None':
            continue
@@ -55,13 +51,12 @@ print("DB접속성공!")
 
 try:
     with connection.cursor() as cursor:
-        #sql_webtoon = """SELECT * FROM `t_webtoon` order by w_no;"""
         sql_webtoon = """select a.w_no, a.w_title, group_concat(c.genre_name separator ', ') as genre_name from t_webtoon A
-left join t_w_genre B
-on a.w_no = b.w_no
-left join t_genre C
-on b.genre_no = c.genre_no
-group by w_no;"""
+                            left join t_w_genre B
+                            on a.w_no = b.w_no
+                            left join t_genre C
+                            on b.genre_no = c.genre_no
+                            group by w_no;"""
         sql_user = """SELECT * FROM `t_comment`"""
         result_webtoon = pd.read_sql(sql_webtoon,connection)
         result_rating = pd.read_sql(sql_user,connection)
@@ -71,15 +66,17 @@ group by w_no;"""
         result_webtoon.w_no = pd.to_numeric(result_webtoon.w_no, errors='coerce')
         result_rating.w_no = pd.to_numeric(result_rating.w_no, errors='coerce')
 
-        data = pd.merge(result_webtoon, result_rating, on='w_no', how='inner')
+        data = pd.merge(result_webtoon, result_rating, on='w_no', how='left')
+        matrix = data.pivot_table(index='u_no', columns='w_no', values='c_rating')          #매트릭스에 빈 컬럼(단 한명도 평점을 주지 않았을 경우)은 포함되지 않음.
 
-        matrix = data.pivot_table(index='u_no', columns='w_no', values='c_rating')
-
-        recomment_result = recommend(w_no,matrix, 10, similar_genre=True)
+        recomment_result = recommend(w_no_args, matrix, 5, similar_genre=True)
 
         result = pd.DataFrame(recomment_result, columns=['w_no','Correlation'])
 
-        print(result)
+        #print(recomment_result)
+
+
+        #print(result)
 
 finally:
     connection.close()
